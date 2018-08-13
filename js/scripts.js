e3e4cabeb5f13dae6d1aa05c4f6517624f8a307c
agsu77 <38299355+agsu77@users.noreply.github.com>
2018-08-13 20:40:27 -0300

    cambio listaceibal, cambio img carousel

--- js/scripts.js
+++ js/scripts.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-        	scrollTop: target.offset().top - 50
+        	scrollTop: target.offset().top - 65 // 60 height navbar 
         }, 1000, function() {
           // Callback after animation
           // Must change focus!
